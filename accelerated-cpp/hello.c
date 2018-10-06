d2329c127b9f515a5d06a2cb8d7145e71a8a2a78
CalciumCat <dcgb11@gmail.com>
2018-10-06 13:21:05 +0100

    exercises from the getting started section

---
+++ accelerated-cpp/hello.c
@@ -0,0 +1,9 @@
+#include <iostream>
+// hello.c outputs Hello world! and some special characters
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+	std::cout << "This is a quote (\") and this is a backslash \\ \n\t <-- that was a tab \n";
+	return 0;
+}
---
+++ accelerated-cpp/helloception.c
@@ -0,0 +1,11 @@
+#include <iostream>
+// helloception prints the "Hello, world!" programme as its output
+
+int main()
+{
+	std::cout << "#include <iostream>";
+	std::cout << "\n// A program that prints \"Hello, world!\" as it's output";
+	std::cout << "\n\nint main()\n{ \n\tstd::cout << \"Hello, world!\" << std::endl;\n}";
+	std::cout << "\nreturn 0;\n";
+	return 0;
+}
