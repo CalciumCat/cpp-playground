6c884b6a8623e2ed8bda3ba4b2125f0fdd76a210
CalciumCat <dcgb11@gmail.com>
2018-10-07 10:03:52 +0100

    user input for padding on frame. Looping ex.

--- accelerated-cpp/frame.c
+++ accelerated-cpp/frame.c
@@ -12,12 +12,22 @@ int main()
 	string noun;
 	cin	>> noun;
 
+	cout <<"Horizontal padding =" << endl;
+	int ncols;
+	cin >> ncols;
+
+	cout <<"Vertical padding =" << endl;
+	int nrows;
+	cin >> nrows;
+
+
 	const string message = noun +"s and where to find them";
 
-	const int row_pad = 1;
-	const int col_pad = 5; 
+	const int row_pad = nrows;
+	const int col_pad = ncols; 
 	const int rows = 3 + row_pad*2;
 	const string::size_type cols = message.size() + 2 + 2*col_pad;
+	const string blank_pad(col_pad, ' ');
 
 	cout << endl;
 	// invariant - written r rows so far
@@ -26,15 +36,18 @@ int main()
 		string::size_type c = 0;
 		// invariant - written c columns so far
 		while( c!=cols ){
-			if( r == row_pad+1 && c == col_pad+1){
-				cout << message;
-				c += message.size();
+			if( r == row_pad+1 && c == 1){
+				cout << blank_pad+message+blank_pad;
+				c+= cols-2;
 			} else {
 				if(r==0 || r==rows-1 ||
 				   c==0 || c==cols-1 )
 					cout << "*";
-				else
+				else {
 					cout << " ";
+					//c = c + blank_line.size()-1;
+				}
+
 				++c;					
 			}
 		}
