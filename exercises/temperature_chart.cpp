83e01a7a857ba12bd5c29d0789e885b9e0a7870e
CalciumCat <dcgb11@gmail.com>
2018-11-30 20:33:42 +0000

    Add functions for input and output of temperature_chart

--- exercises/temperature_chart.cpp
+++ exercises/temperature_chart.cpp
@@ -1,38 +1,70 @@
 /* temperature_chart displays a temperature conversion chart based on  upper and
- lower bounds (Fahrenheit) given by the user. */
+ lower bounds (Fahrenheit) given by the user. The user also determines the 
+ temperature difference between entries in the table. */
 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
 using namespace std;
 
 double to_celcius(int temp);
 double to_absolute_value(int temp);
+
+void print_description_message();
+void user_input(int& minimum, int& maximum, int& step);
+void print_echo_input_message(int minimum, int maximum, int step);
+void print_table(int minimum, int maximum, int step);
 
 /* Main Program */
 int main() {
 	int fahr_minimum = 0;
 	int fahr_maximum = 0;
 	int step = 1;
-	int temp;
-	double celcius;
+
+	// Print message describing the program
+	print_description_message();
+
+	// Request user input for chart specification
+	user_input(fahr_minimum, fahr_maximum, step);
+	// Echo input
+	print_echo_input_message(fahr_minimum, fahr_maximum, step);
+
+	// Print table 
+	print_table(fahr_minimum, fahr_maximum, step);
+
+	return 0;
+}
+/* End of Program */
 
 
-	// Request user input
+/* Function to explain how program works */
+void print_description_message() 
+{
 	cout << " \nThis program generates a temperature conversion chart.  ";
 	cout << "You can choose the lowest and highest temperature, as well as the step size. \n\n"; 
+}
+
+/* Function to prompt user to input specifications for the table */
+void user_input(int& minimum, int& maximum, int& step) 
+{
 	cout << "Enter the lowest, highest and step values of the temperature";
 	cout << "you want in the chart (integers in Fahrenheit): \n\n";
-	cin >> fahr_minimum >> fahr_maximum >> step ;
+	cin >> minimum >> maximum >> step ;
+}
 
-	// Echo input
+/* Function to echo user input */
+void print_echo_input_message(int minimum, int maximum, int step) 
+{
 	cout << "\nConversion chart for temperatures between ";
-	cout << fahr_minimum << " and " << fahr_maximum ;
-	cout << ", calculated every " << step << " Fahrenheit:\n\n";
+	cout << minimum << " and " << maximum ;
+	cout << ", calculated every " << step << " Fahrenheit:\n\n";	
+}
 
+/* Function to print conversion table */
+void print_table(int minimum, int maximum, int step)
+{
+	int temp;
 
-	/* Print headings */
 	cout << setiosflags ( ios :: left );
 	cout.width(20);
 	cout << "Fahrenheit" ;
@@ -46,26 +78,26 @@ int main() {
 	cout.precision(2);
 
 	// Print table
-	for(temp = fahr_minimum ; temp <= fahr_maximum; temp += step) {
-		celcius = to_celcius(temp);
+	for(temp = minimum ; temp <= maximum; temp += step) {
 		cout.width(20);
 		cout << temp;
 		cout.width(20);
-		cout << celcius;
+		cout <<  to_celcius(temp);
 		cout.width(20);
-		cout << celcius + 273.15 << "\n";
+		cout << to_absolute_value(temp) << "\n";
 	}
-	return 0;
 }
 
-/* End of Program */
 
 /* Function to convert Fahrenheit to Celcius */
-double to_celcius(int temp) {
+double to_celcius(int temp) 
+{
 	return (temp-32)*5.0/9.0;
 }
 
 /* Function to convert Fahrenheit to absolute value */
-double to_absolute_value(int temp) {
+double to_absolute_value(int temp) 
+{
 	return to_celcius(temp)+273.15;
 }
+
