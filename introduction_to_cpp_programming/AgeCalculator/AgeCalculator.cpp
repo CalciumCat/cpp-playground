00a662093bdba556e20e893e33f7f6f129485727
CalciumCat <dcgb11@gmail.com>
2018-12-25 11:35:52 +0000

    Ex 1, Q 3

--- introduction_to_cpp_programming/AgeCalculator/AgeCalculator.cpp
+++ introduction_to_cpp_programming/AgeCalculator/AgeCalculator.cpp
@@ -21,25 +21,55 @@
 	
 	int main() 
 	{ 
-		int year_now, age_now, another_year, another_age; 
+		int year_now, age_now, another_year, another_age_year; 
+		int month_now, month_birth, another_month, another_age_month;
+		string month_string;
+
+		month_string = " month";
 	 
 		cout << "Enter the current year then press RETURN.\n"; 
 		cin >> year_now; 
+
+		cout << "Enter the current month (1-12).\n"; 
+		cin >> month_now;
 	 
 		cout << "Enter your current age in years.\n"; 
 		cin >> age_now; 
+
+		cout << "Enter the month (1-12) in which you were born.\n";
+		cin >> month_birth;
 	 
 		cout << "Enter the year for which you wish to know your age.\n";
 		cin >> another_year; 
+
+		cout << "Enter the month for that year.\n";
+		cin >> another_month;
 	 
-		another_age = another_year - (year_now - age_now); 
+		another_age_year = another_year - (year_now - age_now); 
+		another_age_month = another_month - month_birth;
+
+	// make sure the month is not negative by taking away 1 year and adding 12 months
+		if(another_age_month < 0) {
+			another_age_year--;
+			another_age_month = another_age_month + 12;
+		}
+
+		if(another_age_month > 1) {
+			month_string = " months";
+		}
 	 
-		if (another_age >= 0) {
-			cout << "Your age in " << another_year << ": "; 
-			cout << another_age << "\n";
-		} else { 
+
+		if (another_age_year > 150) {
+			cout << "Sorry, but you'll probably be dead by "<< another_year <<".\n";
+		}
+		else if(another_age_year >= 0) {
+			cout << "Your age in " << another_month <<"/" << another_year << ": "; 
+			cout << another_age_year << " and ";
+			cout << another_age_month << month_string << "\n";
+		} 
+		else { 
 			cout << "You weren't even born in ";
-			cout << another_year << "!\n"; 
+			cout << another_month << "/" << another_year << "!\n"; 
 		}
 	
 		return 0; 
