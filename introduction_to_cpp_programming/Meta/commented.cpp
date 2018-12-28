d00eec0168f5b8cb06b2cc665a1057a1147d38bb
CalciumCat <dcgb11@gmail.com>
2018-12-28 21:19:40 +0000

    Adds function print_without_comments that removes comments marked by /* */

--- introduction_to_cpp_programming/Meta/commented.cpp
+++ introduction_to_cpp_programming/Meta/commented.cpp
@@ -1,4 +1,4 @@
-/* Program commented.cpp outputs itself withougt comments */
+/* Program commented.cpp outputs itself without comments */
 
 #include<iostream>
 #include<fstream>
@@ -6,24 +6,26 @@
 
 using namespace std;
 
-void print_source_to_screen(ifstream& in_stream);
-void print_source_to_file();
+void print_with_comments(ifstream& in_stream);
+void print_without_comments(ifstream& in_stream, ofstream& out_stream);
 
 /*	Start of main program */
 int main() {
 	ifstream in_stream;
+	ofstream out_stream;
 
 	cout << "Testing: " << 16/2 << " = " << 4*2 << ".\n\n";
+	//in_stream.open("commented.cpp");
+	//print_with_comments(in_stream); // closes file
+
 	in_stream.open("commented.cpp");
-	print_source_to_screen(in_stream);
+	out_stream.open("uncommented.cpp");
+	print_without_comments(in_stream, out_stream);
 }
 
-void print_source_to_screen(ifstream& in_stream){
+/*	Function that prints file contents to screen */
+void print_with_comments(ifstream& in_stream){
 	char character;
-	//ifstream in_stream;
-	ofstream out_stream; 
-
-	//in_stream.open("commented.cpp");
 
 	in_stream.get(character);
 
@@ -35,7 +37,48 @@ void print_source_to_screen(ifstream& in_stream){
 	in_stream.close();
 }
 
-void print_source_to_file(){
+/*	Function that prints file contents to a file*/
+void print_without_comments(ifstream& in_stream, ofstream& out_stream){
+	const int False = 0;
+	const int True = 1;
+	char character;
+	char next_character;
+	int inside_comment = False; 
+
+	in_stream.get(character);
+
+	while(!in_stream.eof()) 
+	{
+		/*	Check if there's a comment */
+		if(inside_comment == False && character == '/')
+		{
+			in_stream.get(next_character);
+			if(next_character=='*')
+				inside_comment = True;
+			else
+				in_stream.putback(next_character);
+		}
+
+		/*	Check if at the end of a comment */
+		if(inside_comment == True && character == '*')
+		{
+			in_stream.get(next_character);
+			if(next_character == '/')
+				in_stream.get(character);
+				inside_comment = False;
+
+		}
+
+
+		/*	Write to file and screen */
+		if(inside_comment == False){
+			out_stream.put(character);
+			cout << character;
+		}
+	
+		in_stream.get(character);
+	}
+	in_stream.close();
 
 }
 
