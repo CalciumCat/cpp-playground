d3440a6fb66ead7b9d85ef37397db60afcbbac51
CalciumCat <dcgb11@gmail.com>
2018-12-28 20:21:22 +0000

    begins commented.cpp - a programme that prints itself without comments

---
+++ introduction_to_cpp_programming/Meta/commented.cpp
@@ -0,0 +1,23 @@
+/* Program commented.cpp outputs itself withougt comments */
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+void print_source();
+void print_source_to_file();
+
+/*	Start of main program */
+int main() {
+	cout << "Testing: " << 16/2 << " = " << 4*2 << ".\n\n";
+}
+
+void print_source_to_screen(){
+
+}
+
+void print_source_to_file(){
+
+}
+
